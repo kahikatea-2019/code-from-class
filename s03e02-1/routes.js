9fd25fac3879593f586d4651f10c4afb451ff73f
Bryce Kehoe <bryce@kehoe.co.nz>
2019-04-02 11:36:10 +1300

    Added multiple examples of how to display error, code re-use ftw

--- s03e02-1/routes.js
+++ s03e02-1/routes.js
@@ -6,6 +6,8 @@ const db = require('./db')
 router.get('/', (req, res) => res.render('home'))
 
 router.get('/users', (req, res) => {
+  // This example uses named functions
+  // for each step in the promise chain
   db.getUsers()
     .then(displayUsers)
     .catch(displayError)
@@ -14,12 +16,14 @@ router.get('/users', (req, res) => {
     res.render('users', { users })
   }
 
-  function displayError (err) {
+  function displayError (res, err) {
     res.status(500).send(err.message)
   }
 })
 
 router.get('/posts', (req, res) => {
+  // This example uses in-line anonymous functions
+  // for each step in the promise chain
   db.getPosts()
     .then(posts => res.render('posts', { posts }))
     .catch(err => res.status(500).send(err.message))
@@ -27,9 +31,18 @@ router.get('/posts', (req, res) => {
 
 router.get('/post/:id', (req, res) => {
   const id = req.params.id
+
+  // This example passes the res object
+  // to the global displayError function,
+  // good for code reusability
   db.getPostById(id)
     .then(post => res.render('post', post))
-    .catch(err => res.status(500).send(err.message))
+    .catch(err => displayError(res, err))
 })
+
+// Global function (called on line 40)
+function displayError (res, err) {
+  res.status(500).send(err.message)
+}
 
 module.exports = router
